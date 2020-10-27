7bbda05e9fa377f2874bb47f964502e8cdcd61c4
ao20 <72971207+ao20@users.noreply.github.com>
2020-10-27 16:54:18 +0000

    Update dummy.py

--- branchpro/dummy.py
+++ branchpro/dummy.py
@@ -1,7 +1,11 @@
 #
 # Dummy Class
 #
-
+#
+# This file is part of branchpro (https://github.com/SABS-R3-Epidemiology/branchpro/)
+# which is released under the BSD 3-clause license. See accompanying LICENSE.md
+# for copyright notice and full license details.
+#
 
 class Dummy:
     """Dummy Class:
