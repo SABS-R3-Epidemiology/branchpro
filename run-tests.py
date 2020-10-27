0ac186c37c34c4f3a56a6cecba5d4619d282bd01
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2020-10-27 18:55:57 +0000

    merge new testing

--- run-tests.py
+++ run-tests.py
@@ -10,6 +10,7 @@ import unittest
 import os
 import sys
 import argparse
+import datetime
 
 
 def run_unit_tests():
@@ -21,6 +22,54 @@ def run_unit_tests():
                                                       pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests_suite)
     sys.exit(0 if result.wasSuccessful() else 1)
+
+
+# This function is from the Pints library
+# https://github.com/pints-team/pints/blob/master/run-tests.py
+def run_copyright_checks():
+    """
+    Checks that the copyright year in LICENSE.md is up-to-date and that each
+    file contains the copyright header
+    """
+    print('\nChecking that copyright is up-to-date and complete.')
+
+    year_check = True
+    current_year = str(datetime.datetime.now().year)
+
+    with open('LICENSE.md', 'r') as license_file:
+        license_text = license_file.read()
+        if 'Copyright (c) ' + current_year in license_text:
+            print("Copyright notice in LICENSE.md is up-to-date.")
+        else:
+            print('Copyright notice in LICENSE.md is NOT up-to-date.')
+            year_check = False
+
+    # Recursively walk the pints directory and check copyright header is in
+    # each checked file type
+    header_check = True
+    checked_file_types = ['.py']
+    copyright_header = """#
+# This file is part of BRANCHPRO
+# (https://github.com/SABS-R3-Epidemiology/branchpro.git) which is released
+# under the BSD 3-clause license. See accompanying LICENSE.md for copyright
+# notice and full license details.
+#"""
+
+    for dirname, subdir_list, file_list in os.walk('branchpro'):
+        for f_name in file_list:
+            if any([f_name.endswith(x) for x in checked_file_types]):
+                path = os.path.join(dirname, f_name)
+                with open(path, 'r') as f:
+                    if copyright_header not in f.read():
+                        print('Copyright blurb missing from ' + path)
+                        header_check = False
+
+    if header_check:
+        print('All files contain copyright header.')
+
+    if not year_check or not header_check:
+        print('FAILED')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
@@ -37,6 +86,12 @@ if __name__ == '__main__':
         help='Run all unit tests using `python` interpretor.',
     )
 
+    parser.add_argument(
+        '--copyright',
+        action='store_true',
+        help='Check that copyright license info is up to date.',
+    )
+
     # Parse!
     args = parser.parse_args()
 
@@ -46,6 +101,9 @@ if __name__ == '__main__':
     if args.unit:
         has_run = True
         run_unit_tests()
+    if args.copyright:
+        has_run = True
+        run_copyright_checks()
 
     if not has_run:
         parser.print_help()
