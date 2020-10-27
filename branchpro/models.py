80664656cb3d649889918cad90b0c8bf005a6b6a
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-10-27 17:59:47 +0000

    Create initial BranchProModel

--- branchpro/models.py
+++ branchpro/models.py
@@ -1,6 +1,7 @@
 #
 # ForwardModel Class
 #
+import numpy as np
 
 
 class ForwardModel(object):
@@ -43,7 +44,7 @@ class ForwardModel(object):
 #
 
 
-class BranchProModel:
+class BranchProModel(ForwardModel):
     """BranchProModel Class:
 
     Parameters
@@ -52,5 +53,35 @@ class BranchProModel:
         example of value
     """
 
-    def __init__(self, value=5):
-        self.value = value
+    def __init__(self, initial_r, serial_interval):
+        super(BranchProModel, self).__init__()
+
+        if np.sum(serial_interval) <= 0:
+            raise ValueError('Sum of serial interval values must be > 0')
+
+        self._serial_interval = np.asarray(serial_interval)
+        self._initial_r = initial_r
+
+    def __normalised_daily_mean__(self, t, incidences, reproduction_num, serial_interval):  # noqa
+        return reproduction_num * sum([incidences[t - s] * serial_interval[s - 1] for s in range(t)]) / np.sum(serial_interval)  # noqa
+
+    def simulate(self, parameters, times):
+        initial_cond = parameters
+        reproduction_num = self._initial_r
+
+        # Initialise list of number of cases per unit time
+        # with initial condition I0
+        last_time_point = np.max(times)
+        incidences = np.empty(shape=last_time_point + 1)
+        incidences[0] = initial_cond
+
+        # Construct simulation times in steps of 1 unit time each
+        simulation_times = np.arange(start=1, stop=last_time_point, step=1)
+
+        # Compute normalised daily means for full timespan
+        # and draw samples for the incidences
+        for t in simulation_times:
+            norm_daily_mean = self.__normalised_daily_mean__(t, incidences, reproduction_num, serial_interval)  # noqa
+            incidences[t] = np.random.poisson(lam=norm_daily_mean, size=1)
+
+        return incidences[np.in1d(np.append(np.asarray(0), simulation_times), times)]  # noqa
