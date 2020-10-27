f86f9b88afd42ecddcb68c7d9ba3faeaa91259b0
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-10-27 08:37:41 +0000

    Modify init file for pull request to work

--- branchpro/__init__.py
+++ branchpro/__init__.py
@@ -7,6 +7,4 @@ number of cases of infections by day during an outbreak of the influenza virus.
 from .version_info import VERSION_INT, VERSION  # noqa
 
 # Import main classes
-from .models import ForwardModel, BranchProModel    # noqa
-from .simulation import SimulationController    # noqa
-from .visualisation import Visualisation     # noqa
+from .dummy import Dummy    # noqa
