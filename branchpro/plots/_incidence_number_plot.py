eef97e4673d20b530f83b9e5c19b20980bb65159
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-11-25 17:28:07 +0000

    Initial commits for IncidenceNumbePlot class

---
+++ branchpro/plots/_incidence_number_plot.py
@@ -0,0 +1,45 @@
+#
+# IncidenceNumberPlot Class
+#
+# This file is part of BRANCHPRO
+# (https://github.com/SABS-R3-Epidemiology/branchpro.git) which is released
+# under the BSD 3-clause license. See accompanying LICENSE.md for copyright
+# notice and full license details.
+#
+
+import plotly.graph_objs as go
+
+
+class IncidenceNumberPlot():
+    """
+    """
+    def __init__(self):
+        self._figure = go.Figure()
+
+    def add_data(self, df, time_key='Time', inc_key='Incidence Number'):
+        trace = go.Bar(
+            y=df[inc_key],
+            x=df[time_key],
+            name='cases'
+        )
+
+        self._figure.add_trace(trace)
+        self._figure.update_layout(
+            xaxis_title=time_key,
+            yaxis_title=inc_key)
+
+    def add_simulation(self, df, time_key='Time', inc_key='Incidence Number'):
+        trace = go.Scatter(
+            y=df[inc_key],
+            x=df[time_key],
+            mode='lines',
+            name='simulation'
+        )
+
+        self._figure.add_trace(trace)
+        self._figure.update_layout(
+            xaxis_title=time_key,
+            yaxis_title=inc_key)
+
+    def show_figure(self):
+        self._figure.show()
---
+++ branchpro/plots/tests/test_incidence_number_plot.py
@@ -0,0 +1,53 @@
+#
+# This file is part of BRANCHPRO
+# (https://github.com/SABS-R3-Epidemiology/branchpro.git) which is released
+# under the BSD 3-clause license. See accompanying LICENSE.md for copyright
+# notice and full license details.
+#
+
+import unittest
+
+import pandas as pd
+
+import branchpro as bp
+
+
+class TestIncidenceNumberPlotClass(unittest.TestCase):
+    """
+    Test the 'IncidenceNumberPlot' class.
+    """
+    def test__init__(self):
+        bp.IncidenceNumberPlot()
+
+    def test_add_data(self):
+        df = pd.DataFrame({
+            "Time": [1, 2, 3, 5, 6],
+            "Incidence Number": [10, 3, 4, 6, 9]
+        })
+        my_plot = bp.IncidenceNumberPlot()
+        my_plot.add_data(df)
+
+    def test_add_simulation(self):
+        df = pd.DataFrame({
+            "Time": [1, 2, 3, 5, 6],
+            "Incidence Number": [10, 3, 4, 6, 9]
+        })
+        my_plot = bp.IncidenceNumberPlot()
+        my_plot.add_data(df)
+
+        dfs = pd.DataFrame({
+            "Time": [1, 2, 4, 5, 6],
+            "Incidence Number": [2, 3, 8, 10, 5]
+        })
+
+        my_plot.add_simulation(dfs)
+
+    def test_show_figure(self):
+        df = pd.DataFrame({
+            "Time": [1, 2, 3, 5, 6],
+            "Incidence Number": [10, 3, 4, 6, 9]
+        })
+        my_plot = bp.IncidenceNumberPlot()
+        my_plot.add_data(df)
+
+        my_plot.show_figure()
