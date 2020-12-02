66d9260ad5983cacb4ee66c28ef6f512192293cd
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-12-02 17:35:23 +0000

    Add docstrings to _incidence_number_plot.py (with Andrew)

--- branchpro/_incidence_number_plot.py
+++ branchpro/_incidence_number_plot.py
@@ -12,12 +12,25 @@ import plotly.graph_objs as go
 
 
 class IncidenceNumberPlot():
-    """
+    """IncidenceNumberPlot Class
+    Stores the main figure for the Dash app.
     """
     def __init__(self):
         self._figure = go.Figure()
 
     def add_data(self, df, time_key='Time', inc_key='Incidence Number'):
+        """
+        Supplies data to the figure which will be used for the bar plot.
+
+        Parameters
+        ----------
+        df
+            (pandas DataFrame) contains numbers of new cases by days.
+        time_key
+            x-axis label for the bar plot.
+        inc_key
+            y-axis label for the bar plot.
+        """
         if type(df) != pd.DataFrame:
             raise TypeError('Data needs to be a dataframe')
 
@@ -33,6 +46,19 @@ class IncidenceNumberPlot():
             yaxis_title=inc_key)
 
     def add_simulation(self, df, time_key='Time', inc_key='Incidence Number'):
+        """
+        Supplies simulated data to the figure which will be added the bar plot
+        as lines and used for comparison with observed data.
+
+        Parameters
+        ----------
+        df
+            (pandas DataFrame) contains numbers of new cases by days.
+        time_key
+            x-axis label for the bar plot.
+        inc_key
+            y-axis label for the bar plot.
+        """
         if type(df) != pd.DataFrame:
             raise TypeError('Simulation needs to be a dataframe')
 
@@ -49,4 +75,7 @@ class IncidenceNumberPlot():
             yaxis_title=inc_key)
 
     def show_figure(self):
+        """
+        Shows current figure object in the Dash app.
+        """
         self._figure.show()
