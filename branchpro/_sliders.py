Fix precision of marks

branchpro/_sliders.py
@@ -40,13 +40,13 @@ class _SliderComponent():
         step_size
             Incremement between slider values.
         """
-        marks_list = np.arange(
+        mark_list = np.arange(
                                 start=min_val,
                                 stop=max_val+step_size,
                                 step=step_size
                                 )
-        if marks_list.size > 11:
-            marks_list = np.linspace(
+        if mark_list.size > 11:
+            mark_list = np.linspace(
                                         start=min_val,
                                         stop=max_val,
                                         num=11
@@ -59,7 +59,9 @@ class _SliderComponent():
                             min=min_val,
                             max=max_val,
                             value=init_val,
-                            marks={str(ri): str(ri) for ri in marks_list},
+                            marks={
+                                str(r): '{:.2f}'.format(r) for r in mark_list
+                                },
                             step=step_size
                         )
                     ]

branchpro/tests/test_sliders.py
@@ -30,7 +30,7 @@ class Test_SliderComponent(unittest.TestCase):
         self.assertEqual(sliders._sliders[1].value, 0)
         self.assertEqual(
             sliders._sliders[1].marks,
-            {str(ri): str(ri) for ri in [0.0, 0.5, 1.0]}
+            {str(ri): '{:.2f}'.format(ri) for ri in [0.00, 0.50, 1.00]}
             )
         self.assertEqual(sliders._sliders[1].step, 0.5)
 
@@ -41,7 +41,7 @@ class Test_SliderComponent(unittest.TestCase):
         self.assertEqual(sliders._sliders[3].value, 0)
         self.assertEqual(
             sliders._sliders[3].marks,
-            {str(ri): str(ri) for ri in np.linspace(0, 1, 11)}
+            {str(ri): '{:.2f}'.format(ri) for ri in np.linspace(0, 1, 11)}
             )
         self.assertEqual(sliders._sliders[3].step, 0.05)
 
@@ -58,7 +58,7 @@ class Test_SliderComponent(unittest.TestCase):
         self.assertEqual(div.children[1].value, 0)
         self.assertEqual(
             div.children[1].marks,
-            {str(ri): str(ri) for ri in [0.0, 0.5, 1.0]}
+            {str(ri): '{:.2f}'.format(ri) for ri in [0.00, 0.50, 1.00]}
             )
         self.assertEqual(div.children[1].step, 0.5)
 
@@ -69,7 +69,8 @@ class Test_SliderComponent(unittest.TestCase):
         self.assertEqual(div.children[3].value, 0.5)
         self.assertEqual(
             div.children[3].marks,
-            {str(ri): str(ri) for ri in [0.0, 0.25, 0.5, 0.75, 1.0]}
+            {str(ri): '{:.2f}'.format(ri) for ri in [
+                0.00, 0.25, 0.50, 0.75, 1.00]}
         )
         self.assertEqual(div.children[3].step, 0.25)
 