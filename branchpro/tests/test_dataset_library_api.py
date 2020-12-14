a347260f1ef1e0c4f2f28a831a8dd5e2ed67bebf
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-12-14 19:19:23 +0200

    Create test_datalibapi.py

---
+++ branchpro/tests/test_dataset_library_api.py
@@ -0,0 +1,22 @@
+#
+# This file is part of BRANCHPRO
+# (https://github.com/SABS-R3-Epidemiology/branchpro.git) which is released
+# under the BSD 3-clause license. See accompanying LICENSE.md for copyright
+# notice and full license details.
+#
+
+import unittest
+from unittest.mock import patch
+
+import pandas as pd
+
+import branchpro as bp
+
+
+class TestDatasetLibraryAPIClass(unittest.TestCase):
+    """
+    Test the 'DatasetLibraryAPI' class.
+    """
+    def test__init__(self):
+        bp.DatasetLibraryAPI()
+
