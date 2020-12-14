d1231f884f1cc83e08709755c4af38a214d201a3
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-12-14 19:19:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     install_requires=[
         # Dependencies go here!
         'numpy>=1.8',
-        'dash',
+        'dash>=1.18',
         'dash_bootstrap_components',
         'pandas',
         'plotly'
