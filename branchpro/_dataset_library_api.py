09b76650fa22fa102f239d96e06faea5ef344135
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-12-15 16:20:16 +0200

    Update docstring

--- branchpro/_dataset_library_api.py
+++ branchpro/_dataset_library_api.py
@@ -29,18 +29,27 @@ class DatasetLibrary(object):
 
         The data is returned as a :class:`pandas.DataFrame` with columns
         `time_index`: index of the time point represented in the data file;
+
         `year`: year of that particular incidence value;
+
         `week`: week of that particular incidence value;
+
         `day`: day of that particular incidence value;
+
         `inc`: number of flu cases at that time point;
+
         `inc_low`: lower bound of the 95% confidence interval of the number
         of flu cases at that time point;
+
         `inc_up`: upper bound of the 95% confidence interval of the number
         of flu cases at that time point;
+
         `inc100`: number of flu cases at that time point reported to
         100,000 people;
+
         `inc100_low`: lower bound of the 95% confidence interval of the number
         of flu cases at that time point reported to 100,000 people;
+
         `inc100_up`: upper bound of the 95% confidence interval of the number
         of flu cases at that time point reported to 100,000 people.
         """
