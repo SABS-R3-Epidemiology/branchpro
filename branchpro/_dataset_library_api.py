3c000f9ae9e65e95ef5568263a56e61d5c5ecacc
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-12-15 15:30:19 +0200

    Update doctring

--- branchpro/_dataset_library_api.py
+++ branchpro/_dataset_library_api.py
@@ -28,31 +28,21 @@ class DatasetLibrary(object):
         outbreak in France between the years 1984 and 2020.
 
         The data is returned as a :class:`pandas.DataFrame` with columns
-        `time_index`
-            index of the time point represented in the data file;
-        `year`
-            year of that particular incidence value;
-        `week`
-            week of that particular incidence value;
-        `day`
-            day of that particular incidence value;
-        `inc`
-            number of flu cases at that time point;
-        `inc_low`
-            lower bound of the 95% confidence interval of the number
-            of flu cases at that time point;
-        `inc_up`
-            upper bound of the 95% confidence interval of the number
-            of flu cases at that time point;
-        `inc100`
-            number of flu cases at that time point reported to
-            100,000 people;
-        `inc100_low`
-            lower bound of the 95% confidence interval of the number
-            of flu cases at that time point reported to 100,000 people;
-        `inc100_up`
-            upper bound of the 95% confidence interval of the number
-            of flu cases at that time point reported to 100,000 people.
+        `time_index`: index of the time point represented in the data file;
+        `year`: year of that particular incidence value;
+        `week`: week of that particular incidence value;
+        `day`: day of that particular incidence value;
+        `inc`: number of flu cases at that time point;
+        `inc_low`: lower bound of the 95% confidence interval of the number
+        of flu cases at that time point;
+        `inc_up`: upper bound of the 95% confidence interval of the number
+        of flu cases at that time point;
+        `inc100`: number of flu cases at that time point reported to
+        100,000 people;
+        `inc100_low`: lower bound of the 95% confidence interval of the number
+        of flu cases at that time point reported to 100,000 people;
+        `inc100_up`: upper bound of the 95% confidence interval of the number
+        of flu cases at that time point reported to 100,000 people.
         """
         filepath = os.path.join(self._directory, 'french_flu_data.csv')
         return pd.read_csv(filepath)
