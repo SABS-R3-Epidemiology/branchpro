1b10b3c58fd8529b28d5c05aa906e1902a660390
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-12-16 11:20:14 +0200

    Add method for changing figure labels

--- branchpro/apps/_incidence_number_plot.py
+++ branchpro/apps/_incidence_number_plot.py
@@ -87,6 +87,21 @@ class IncidenceNumberPlot():
             yaxis_title=inc_key,
             hovermode='x unified')
 
+    def update_labels(self, time_label='Time', inc_label='Incidence Number'):
+        """
+        Updates the figure labels with user inputed values.
+
+        Parameters
+        ----------
+        time_label
+            x-axis label for the bar plot.
+        inc_label
+            y-axis label for the bar plot.
+        """
+        self.figure.update_layout(
+            xaxis_title=time_label,
+            yaxis_title=inc_label)
+
     def show_figure(self):
         """
         Shows current figure.
