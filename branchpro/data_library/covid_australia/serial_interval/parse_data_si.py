758b569d36560df98435d4724ef6bd96f6299c2b
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-03-17 12:13:27 +0000

    Add copyright

--- branchpro/data_library/covid_australia/serial_interval/parse_data_si.py
+++ branchpro/data_library/covid_australia/serial_interval/parse_data_si.py
@@ -1,3 +1,10 @@
+#
+# This file is part of BRANCHPRO
+# (https://github.com/SABS-R3-Epidemiology/branchpro.git) which is released
+# under the BSD 3-clause license. See accompanying LICENSE.md for copyright
+# notice and full license details.
+#
+
 """Processing script for Australian serial intervaldata from [1]_.
 
 It generating multiple serial intervals from a lognormal distribution
