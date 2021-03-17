f674d09bd6a0e2afd5fc8a2ede35868b23abf0b5
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-03-17 12:08:57 +0000

    Create multiple si file dor the analysis

---
+++ branchpro/data_library/covid_australia/serial_interval/parse_data_si.py
@@ -0,0 +1,58 @@
+"""Processing script for Australian serial intervaldata from [1]_.
+
+It generating multiple serial intervals from a lognormal distribution
+with parameetrs as shown in the EpiNow repository:
+https://github.com/epiforecasts/EpiNow/tree/master/data-raw.
+
+References
+----------
+.. [1] Price, David J., et al. "Early analysis of the Australian COVID-19
+       epidemic." Elife 9 (2020): e58785.
+       https://elifesciences.org/articles/58785/figures#content
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import scipy as sc
+import scipy.stats
+
+
+def write_ser_int_data(name):
+    """Write a new csv file for 60-day-long serial intervals
+    to be used for the analysis of the Australian data.
+
+    Parameters
+    ----------
+    name
+        Name given to the serial intervals file.
+
+    """
+    # Select data from the given state
+    path = os.path.join(
+            os.path.dirname(__file__), 'lognormal_param.csv')
+    data = pd.read_csv(path, dtype=np.float64)
+
+    # Split it into param1 and param2 of the lognormal distribution
+    # Keep only 1000 of the pairs
+    s_data = data['param1'].to_numpy()[:1000]
+    scale_data = data['param2'].to_numpy()[:1000]
+
+    si_data = np.zeros((60, 1000))
+
+    # Compute lognormal pdf for given paired parameters and
+    # Discretize distribution and record it
+    for i, _ in enumerate(s_data):
+        w_dist = sc.stats.lognorm(
+            s=s_data[i], scale=np.exp(scale_data[i]))
+        disc_w = w_dist.pdf(np.arange(1, 61))
+        si_data[:, i] = disc_w
+
+    # Transform recorded matrix of serial intervals to csv file
+    path = os.path.join(
+            os.path.dirname(__file__), '{}.csv'.format(name))
+    np.savetxt(path, si_data, delimiter=',')
+
+
+if __name__ == '__main__':
+    write_ser_int_data('si-epinow')
