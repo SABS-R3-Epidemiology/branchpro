14b3c57c6c194f37a7b990830e2758376394ab34
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-03-31 15:08:47 +0100

    Created parse file for the Ontario data

--- branchpro/data_library/covid_ontario/parse_data.py
+++ branchpro/data_library/covid_ontario/parse_data.py
@@ -17,6 +17,8 @@ References
        https://elifesciences.org/articles/58785/figures#content
 """
 
+import time
+from time import mktime
 import datetime
 import os
 import pandas
@@ -45,32 +47,39 @@ def write_state_data(start_date='Sun Mar 01 2020', end_date='Wed Mar 17 2021'):
     imported_data = \
         data[['Travel', 'date']]
     local_data = data[
-        ['Close contact', 'Community spread', 'Outbreak setting',
-         'Other', 'date']]
+        ['Close contact', 'Community spread', 'Outbreak setting', 'date']]
     unknown_data = data[['Other', 'date']]
 
+    # Add up the columns all the the locally produced cases by date
+    local_data['Close contact'] = local_data['Close contact'] + local_data[
+        'Community spread'] + local_data['Outbreak setting']
+
     # Rename columns to the names we are using in the app
-    imported_data = imported_data.rename(columns={'cases': 'Imported Cases'})
-    local_data = local_data.rename(columns={'cases': 'Incidence Number'})
-    unknown_data = unknown_data.rename(columns={'cases': 'Unknown Cases'})
+    imported_data = imported_data.rename(columns={'Travel': 'Imported Cases'})
+    local_data = local_data.rename(
+        columns={'Close contact': 'Incidence Number'})
+    unknown_data = unknown_data.rename(columns={'Other': 'Unknown Cases'})
 
     # Join all sets of cases on date
     data = pandas.merge(local_data, imported_data, on='date', how='outer') \
                  .merge(unknown_data, on='date', how='outer')
     data = data.fillna(value=0)
-    data = data.sort_values('date')
 
     # Treat unknown cases as local
     data['Incidence Number'] = data['Incidence Number'] + data['Unknown Cases']
 
+    # Process dates
+    data['processed-date'] = [process_dates(x) for x in data['date']]
+    data = data.sort_values('processed-date')
+
     # Keep only those values within the date range
-    data = data[data['date'] >= start_date]
-    data = data[data['date'] <= end_date]
+    data = data[data['processed-date'] >= process_dates(start_date)]
+    data = data[data['processed-date'] <= process_dates(end_date)]
 
     # Add a column 'Time', which is the number of days from start_date
-    start = datetime.date(*map(int, start_date.split('-')))
-    data['Time'] = [(datetime.date(*map(int, x.split('-'))) - start).days + 1
-                    for x in data['date']]
+    start = process_dates(start_date)
+    data['Time'] = [(x - start).days + 1
+                    for x in data['processed-date']]
 
     # Keep only those columns we are using
     data = data[['Time', 'Incidence Number', 'Imported Cases', 'date']]
@@ -78,6 +87,11 @@ def write_state_data(start_date='Sun Mar 01 2020', end_date='Wed Mar 17 2021'):
     data.to_csv(
         os.path.join(os.path.dirname(__file__), 'ON.csv'),
         index=False)
+
+
+def process_dates(date):
+    struct = time.strptime(date, '%a %b %d %Y')
+    return datetime.datetime.fromtimestamp(mktime(struct))
 
 
 if __name__ == '__main__':
