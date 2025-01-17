8857f239d82486f8008945fe5a892d870494206d
I-Bouros <ioana.bouros@dtc.ox.ac.uk>
2025-01-17 13:36:04 +0000

    eliminate httspan dependency

--- setup.py
+++ setup.py
@@ -86,8 +86,7 @@ setup(
         'fast_poibin==0.3.1',
         'pystan==3.7.0',
         'arviz==0.16.1',
-        'seaborn==0.13',
-        'httpstan==4.10.1'
+        'seaborn==0.13'
     ],
     python_requires='>=3.9',
     extras_require={
